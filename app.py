fef74a73c323d1a4ce6c0601e37f6988fb4370de
Yuanpeng Zhang <zyroc1990@gmail.com>
2025-02-24 12:34:57 -0500

    fix height for multiple files mode

--- app.py
+++ app.py
@@ -189,7 +189,7 @@ def plot(filename, filename_i):
 
         fig.update_layout(
             legend_title='Data',
-            height=600
+            height=800
         )
         fig.update_xaxes(title_text='X')
         fig.update_yaxes(title_text='Y')
